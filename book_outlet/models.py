b76d10db210fe96f4558d61713de1ce1d31acf20
rahulp <rahulp@qburst.com>
2025-01-22 13:01:58 +0530

    Add slug instead of id in url

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)]),
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=True)
+    slug = models.SlugField(default="", null=False)
 
     def get_absolute_url(self):
-        return reverse("book_detail", args=[self.id])
+        return reverse("book_detail", args=[self.slug])
     
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} {self.author}"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -10,6 +10,6 @@ def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {"books": books})
 
-def details(request, id):
-    book = get_object_or_404(Book, id=id)
+def details(request, slug):
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/details.html", {"title": book.title, "author": book.author, "is_bestseller": book.is_bestselling})
