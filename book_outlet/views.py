b7972c783e3b8c927f69a3b390d68b287ac26ef1
rahulp <rahulp@qburst.com>
2025-01-22 12:09:57 +0530

    Make use of get_object_or_404

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from book_outlet.models import Book
 from django.http import Http404, HttpResponseNotFound
 from django.template.loader import render_to_string
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -10,8 +11,5 @@ def index(request):
     return render(request, "book_outlet/index.html", {"books": books})
 
 def details(request, id):
-    try:
-        book = Book.objects.filter(id=id)[0]
-    except:
-        return HttpResponseNotFound(render_to_string("404.html"))
+    book = get_object_or_404(Book, id=id)
     return render(request, "book_outlet/details.html", {"title": book.title, "author": book.author, "is_bestseller": book.is_bestselling})
