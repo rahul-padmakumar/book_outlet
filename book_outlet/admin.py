ad3fae661c9b68626944ee733465c0e0766d59bb
rahulp <rahulp@qburst.com>
2025-01-24 15:06:38 +0530

    Configure Prepopulated field in admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    readonly_fields = ("slug",)
+    prepopulated_fields = {"slug":("title",)}
 
 admin.site.register(Book, BookAdmin)
