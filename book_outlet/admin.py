96c4b1b5527cd604b250657b5e45741c97ef3033
rahulp <rahulp@qburst.com>
2025-01-24 15:12:17 +0530

    Add filter in admin site

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -5,5 +5,6 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
+    list_filter = ("author","rating",)
 
 admin.site.register(Book, BookAdmin)
