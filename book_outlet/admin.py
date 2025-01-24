a37072559048b164fdc87497cd26877f1e2cf379
rahulp <rahulp@qburst.com>
2025-01-24 15:02:24 +0530

    Setting field as read only in admin site.

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
+
+admin.site.register(Book, BookAdmin)
